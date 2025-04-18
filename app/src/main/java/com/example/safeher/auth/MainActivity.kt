b8ea46bdf93b35c2c60035adf5572cc88294b22c
Amit Ben Yaakov <amitby98@gmail.com>
2025-04-18 17:36:34 +0300

    added Home page fragment

---
+++ app/src/main/java/com/example/safeher/auth/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.safeher.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.safeher.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Obtain reference to the NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // Get the NavController
+        navController = navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+}
---
+++ app/src/main/java/com/example/safeher/auth/authFragments/HomePageFragment.kt
@@ -0,0 +1,76 @@
+package com.example.safeher.auth.authFragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.safeher.R
+import com.example.safeher.auth.authViewModel.AuthViewModel
+import com.example.safeher.general.REMEMBER_MY_LOGIN
+import com.example.safeher.general.SharedPrefsHelper
+import com.example.safeher.mainApp.MainAppActivity
+import com.google.android.material.button.MaterialButton
+
+const val LOGIN = 1
+const val REGISTER = 2
+
+
+class HomePageFragment : Fragment() {
+    private lateinit var mMainLottieImage: AppCompatImageView
+    private lateinit var mMainTitle: AppCompatTextView
+    private lateinit var mMainSubtitle: AppCompatTextView
+    private lateinit var mLoginButton: MaterialButton
+    private lateinit var mSignupButton: MaterialButton
+    private val viewModel: AuthViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home_page, container, false)
+        mMainLottieImage = view.findViewById(R.id.main_activity_mainpage_lottie)
+        mMainTitle = view.findViewById(R.id.main_activity_title)
+        mMainSubtitle = view.findViewById(R.id.main_activity_subtitle)
+        mLoginButton = view.findViewById(R.id.main_activity_login_button)
+        mSignupButton = view.findViewById(R.id.main_activity_signup_button)
+        if(viewModel.getCurrentUser()
+            && SharedPrefsHelper(requireContext()).get(REMEMBER_MY_LOGIN, false)
+        ) {
+            activity?.startActivity(Intent(requireActivity(), MainAppActivity::class.java))
+        }
+        setupClickListeners()
+
+        return view
+    }
+
+    private fun setupClickListeners() {
+        mLoginButton.setOnClickListener {
+            handleButtonClick(LOGIN)
+        }
+
+
+        mSignupButton.setOnClickListener {
+            handleButtonClick(REGISTER)
+        }
+    }
+
+    private fun handleButtonClick(moveTo: Int) {
+        when(moveTo) {
+            LOGIN -> findNavController().navigate(R.id.action_homePageFragment_to_loginFragment)
+            REGISTER -> findNavController().navigate(R.id.action_homePageFragment_to_registerFragment)
+        }
+    }
+
+    companion object {
+        fun newInstance() = HomePageFragment()
+    }
+
+}
