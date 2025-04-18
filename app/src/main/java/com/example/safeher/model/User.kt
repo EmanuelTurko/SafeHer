21f9a10c6553b1fb2d31ea3aa3daa916aa37b72b
Amit Ben Yaakov <amitby98@gmail.com>
2025-04-18 17:39:00 +0300

    added User model

---
+++ app/src/main/java/com/example/safeher/model/User.kt
@@ -0,0 +1,10 @@
+package com.example.safeher.model
+
+data class User(
+    val id: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val birthDate: String = "",
+    val email: String = "",
+    val profileImage: String = ""
+)
